86a9cc8c7c3b9590a3fcf26c444e7fdc1d374043
xavierzhou <xiayezhou@gmail.com>
2019-08-10 20:30:52 +0800

    Updates

---
+++ precache-manifest.2258d298b3e010fba1de2cfa888d47b1.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a81b690262a2121feac9980efc331a4",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-italic.8a81b690.woff"
+  },
+  {
+    "revision": "daf24cd2acefe036e247",
+    "url": "/talk_git_branch_model/static/js/main.daf24cd2.chunk.js"
+  },
+  {
+    "revision": "1e8b5593c629535730765184a7a45643",
+    "url": "/talk_git_branch_model/static/media/branch_model.1e8b5593.png"
+  },
+  {
+    "revision": "1358678163ae642368a0",
+    "url": "/talk_git_branch_model/static/js/runtime~main.13586781.js"
+  },
+  {
+    "revision": "f2d96cb61d6922bd3d1d11088c58a8a6",
+    "url": "/talk_git_branch_model/static/media/montserrat-v7-latin-700.f2d96cb6.eot"
+  },
+  {
+    "revision": "64b9c150ed51d202fe159c07d916966c",
+    "url": "/talk_git_branch_model/static/media/montserrat-v7-latin-700.64b9c150.woff2"
+  },
+  {
+    "revision": "916a3d6c6aab56b8e39cca4f2da65e4d",
+    "url": "/talk_git_branch_model/static/media/montserrat-v7-latin-700.916a3d6c.woff"
+  },
+  {
+    "revision": "e64cab167bbdc04807429d10873901a0",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-regular.e64cab16.woff2"
+  },
+  {
+    "revision": "2868c329f2c9de786cbc4a5b6989ae08",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-italic.2868c329.woff2"
+  },
+  {
+    "revision": "73437ad44345f889f45e27c428d618d8",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-700.73437ad4.woff2"
+  },
+  {
+    "revision": "95c97d73be1b5ac4efa43cd21305e00f",
+    "url": "/talk_git_branch_model/static/media/fira-mono-v4-latin-regular.95c97d73.woff2"
+  },
+  {
+    "revision": "faaa50950e6901eb6f918575d4801807",
+    "url": "/talk_git_branch_model/static/media/fira-mono-v4-latin-regular.faaa5095.eot"
+  },
+  {
+    "revision": "4b672347c9e658b24ac5467118edb584",
+    "url": "/talk_git_branch_model/static/media/montserrat-v7-latin-700.4b672347.ttf"
+  },
+  {
+    "revision": "c35ad7c76819575781c833319c13711b",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-regular.c35ad7c7.eot"
+  },
+  {
+    "revision": "39c4767ac94d2488b975d8d52a4d760e",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-italic.39c4767a.eot"
+  },
+  {
+    "revision": "ce659615885f33d928eb7fe276574106",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-regular.ce659615.woff"
+  },
+  {
+    "revision": "6bd1e07d719554e74fbb7f831c549aa2",
+    "url": "/talk_git_branch_model/static/media/hotfix_2.6bd1e07d.png"
+  },
+  {
+    "revision": "813c58a5156a3f3caddec473846f776f",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-700.813c58a5.eot"
+  },
+  {
+    "revision": "72862e7cf19603ad24f26baf86dd0e08",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-700.72862e7c.woff"
+  },
+  {
+    "revision": "a713797f214a7b87c775e836994cd535",
+    "url": "/talk_git_branch_model/static/media/fira-mono-v4-latin-regular.a713797f.woff"
+  },
+  {
+    "revision": "7621d1489de7a1c7468a5ed6a8065331",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-italic.7621d148.ttf"
+  },
+  {
+    "revision": "953de7bafd9fdbf41ea443aacabe2706",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-regular.953de7ba.ttf"
+  },
+  {
+    "revision": "0161d436c9971da7f6c40f26b5edf22f",
+    "url": "/talk_git_branch_model/static/media/fira-mono-v4-latin-regular.0161d436.ttf"
+  },
+  {
+    "revision": "bdafb9df42d16395dd5d87d12a74ea3f",
+    "url": "/talk_git_branch_model/static/media/open-sans-v13-latin-700.bdafb9df.ttf"
+  },
+  {
+    "revision": "0e5ef4794631e23173a982f5e3c0ab7c",
+    "url": "/talk_git_branch_model/static/media/feat_back_1.0e5ef479.png"
+  },
+  {
+    "revision": "31d9da2c192b21117c2fbf6b64fbe6c6",
+    "url": "/talk_git_branch_model/static/media/feat_back_2.31d9da2c.png"
+  },
+  {
+    "revision": "de77cf39897c8a99a7f8e4ce9716013a",
+    "url": "/talk_git_branch_model/static/media/rc_to_master.de77cf39.png"
+  },
+  {
+    "revision": "e1d6eab4ddee5f4edeb6020c13483769",
+    "url": "/talk_git_branch_model/static/media/merge_master.e1d6eab4.png"
+  },
+  {
+    "revision": "d490a266cfb864607e33a05de4e220ca",
+    "url": "/talk_git_branch_model/static/media/rebase_master.d490a266.png"
+  },
+  {
+    "revision": "bebbe10f6233efa02754b4e8cbee804d",
+    "url": "/talk_git_branch_model/static/media/hotfix_1.bebbe10f.png"
+  },
+  {
+    "revision": "fff0b9f05c84a8413e33",
+    "url": "/talk_git_branch_model/static/js/1.fff0b9f0.chunk.js"
+  },
+  {
+    "revision": "fff0b9f05c84a8413e33",
+    "url": "/talk_git_branch_model/static/css/1.37d35559.chunk.css"
+  },
+  {
+    "revision": "3bfeac418ee0d821b19c84cd3bb6af11",
+    "url": "/talk_git_branch_model/index.html"
+  }
+];
