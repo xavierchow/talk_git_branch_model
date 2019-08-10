d7ffb0e97ce196767f72c5596500883f03582d34
xavierzhou <xiayezhou@gmail.com>
2019-08-11 00:25:20 +0800

    chore: set up gh-pages

---
+++ replace.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+source=$1
+png_files=$(cat build/asset-manifest.json | grep png | grep back_1 | awk ' {print $1 }' | sed -E "s/\"|://g")
+
+for i in "${png_files[@]}"
+do
+  echo $i
+  dotEscaped="${i//\./\\.}"
+  escaped="${dotEscaped//\//\\/}"
+  echo $escaped 
+  sed -i E "s/t\.p\+\"${escaped}/t\.p\+\"\.\.\/${escaped}/" $source
+done
