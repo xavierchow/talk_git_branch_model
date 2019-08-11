d14dd241c84ac5395a135ed29d0b4a4840657b94
xavierzhou <xiayezhou@gmail.com>
2019-08-11 14:45:45 +0800

    chore: update replace sh

--- replace.sh
+++ replace.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 source=$1
-png_files=$(cat build/asset-manifest.json | grep png | awk ' {print $1 }' | sed -E "s/\"|://g")
+png_files=($(cat build/asset-manifest.json | grep png | awk ' {print $2 }' | sed -E "s/\"|,//g"))
 
 for i in "${png_files[@]}"
 do
-  echo $i
-  dotEscaped="${i//\./\\.}"
-  escaped="${dotEscaped//\//\\/}"
-  echo $escaped 
-  sed -i E "s/t\.p\+\"${escaped}/t\.p\+\"\.\.\/${escaped}/" $source
+  # echo $i
+  file=$(basename "$i")
+  echo $file
+  escaped="${file//\./\\.}"
+  # echo $escaped 
+  # echo "s/\"static\/media\/${escaped}/\"\.\.\/static\/media\/${escaped}/"
+  sed -i -e "s/\"static\/media\/${escaped}/\"\.\.\/static\/media\/${escaped}/" $source
 done
